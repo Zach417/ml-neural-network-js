6f2df2c64a83d4f7ef944f0f71b0a9cd5309a0ff
Zach Allen <zallen@pension-consultants.com>
2017-01-11 16:21:03 -0600

    added hidden layer to neural net

--- index.js
+++ index.js
@@ -5,20 +5,22 @@ var Trainer = require('./models/trainer');
 var trainer = new Trainer();
 var trainX = trainer.data.getTrainingX();
 var trainY = trainer.data.getTrainingY();
+var testX = trainX;
+var testY = trainY;
 
 // architect neural network
 var neuralNetwork = new NeuralNetwork();
 neuralNetwork.inputLayer.size = trainX[0].length;
-neuralNetwork.addHiddenLayer(3, "sigmoid");
-neuralNetwork.addHiddenLayer(3, "sigmoid");
-neuralNetwork.addHiddenLayer(3, "sigmoid");
+neuralNetwork.addHiddenLayer(5, "sigmoid");
+neuralNetwork.addHiddenLayer(5, "sigmoid");
+neuralNetwork.addHiddenLayer(5, "sigmoid");
 neuralNetwork.outputLayer.size = trainY[0].length;
 neuralNetwork.outputLayer.setActivation("sigmoid");
 neuralNetwork.generate();
 
 // train
 trainer.neuralNetwork = neuralNetwork;
-trainer.scalar = 3.0;
+trainer.scalar = 1.0;
 trainer.lambda = 0.0;
 trainer.train(1000);
 
@@ -27,4 +29,4 @@ console.log("\nTraining data set results");
 trainer.printResults(trainX, trainY);
 
 console.log("\nTesting data set results");
-trainer.printResults([[0,0]], [[0]]);
+trainer.printResults(testX, testY);
