c11dfe6a3f220020404aeb09c2fae7e900a2e676
Zach Allen <zallen@pension-consultants.com>
2017-01-11 15:57:38 -0600

    added lambda paramter

--- index.js
+++ index.js
@@ -3,19 +3,28 @@ var Trainer = require('./models/trainer');
 
 // setup trainer and gather data
 var trainer = new Trainer();
-var trainX = trainer.dataUtility.getTrainingX();
-var trainY = trainer.dataUtility.getTrainingY();
+var trainX = trainer.data.getTrainingX();
+var trainY = trainer.data.getTrainingY();
 
 // architect neural network
 var neuralNetwork = new NeuralNetwork();
 neuralNetwork.inputLayer.size = trainX[0].length;
 neuralNetwork.addHiddenLayer(3, "sigmoid");
+neuralNetwork.addHiddenLayer(3, "sigmoid");
+neuralNetwork.addHiddenLayer(3, "sigmoid");
 neuralNetwork.outputLayer.size = trainY[0].length;
 neuralNetwork.outputLayer.setActivation("sigmoid");
 neuralNetwork.generate();
 
-// train and print results
+// train
 trainer.neuralNetwork = neuralNetwork;
-trainer.scalar = 2;
+trainer.scalar = 3.0;
+trainer.lambda = 0.0;
 trainer.train(1000);
-trainer.printResults();
+
+// print results
+console.log("\nTraining data set results");
+trainer.printResults(trainX, trainY);
+
+console.log("\nTesting data set results");
+trainer.printResults([[0,0]], [[0]]);
