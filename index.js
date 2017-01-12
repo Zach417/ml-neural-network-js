b555dbe23c3cbe30aa0ae559fa2f2dae7319360c
Zach Allen <zallen@pension-consultants.com>
2017-01-12 09:50:49 -0600

    refactored to npm package

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
 var NeuralNetwork = require('./models/neuralnetwork');
 var Trainer = require('./models/trainer');
 
-// setup trainer and gather data
-var trainer = new Trainer();
-var trainX = trainer.data.getTrainingX();
-var trainY = trainer.data.getTrainingY();
-var testX = trainX;
-var testY = trainY;
-
-// architect neural network
-var neuralNetwork = new NeuralNetwork();
-neuralNetwork.inputLayer.size = trainX[0].length;
-neuralNetwork.addHiddenLayer(5, "sigmoid");
-neuralNetwork.addHiddenLayer(5, "sigmoid");
-neuralNetwork.addHiddenLayer(5, "sigmoid");
-neuralNetwork.outputLayer.size = trainY[0].length;
-neuralNetwork.outputLayer.setActivation("sigmoid");
-neuralNetwork.generate();
-
-// train
-trainer.neuralNetwork = neuralNetwork;
-trainer.scalar = 1.0;
-trainer.lambda = 0.0;
-trainer.train(1000);
-
-// print results
-console.log("\nTraining data set results");
-trainer.printResults(trainX, trainY);
-
-console.log("\nTesting data set results");
-trainer.printResults(testX, testY);
+module.exports = {
+  NeuralNetwork: NeuralNetwork,
+  Trainer: Trainer
+}
