dcf9d68848dece046a22965fb91163aaf7c83118
Zach Allen <zallen@pension-consultants.com>
2017-01-17 17:27:14 -0600

    modified test for up to date testing procedure

--- test/index.js
+++ test/index.js
@@ -13,18 +13,19 @@ var testY = trainY;
 var neuralNetwork = new NeuralNetwork();
 neuralNetwork.input.size = trainX[0].length;
 neuralNetwork.input.setActivation("linear");
-neuralNetwork.addHiddenLayer(10, "hyperbolic-tangent");
+neuralNetwork.addHiddenLayer(3, "hyperbolic-tangent");
+neuralNetwork.addHiddenLayer(3, "hyperbolic-tangent");
 neuralNetwork.output.size = trainY[0].length;
 neuralNetwork.output.setActivation("hyperbolic-tangent");
 neuralNetwork.generate();
 
 // train
 trainer.neuralNetwork = neuralNetwork;
-trainer.generateFromFile("sine.json");
+trainer.generateFromFile("cosine.json");
 console.log(trainer.neuralNetwork.forward([[Math.PI], [Math.PI / 2]]));
 trainer.scalar = 0.000001;
 trainer.lambda = 0.00001;
-trainer.goal = 0.01;
+trainer.goal = 0.0001;
 trainer.train(100000);
 trainer.writeToFile();
 
