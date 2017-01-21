afd095e5b06db3dc5e24af41ab19eca13ba24edd
Zach Allen <zallen@pension-consultants.com>
2017-01-21 11:51:39 -0600

    reverted to generic test with generic data set

--- test/index.js
+++ test/index.js
@@ -1,35 +1,37 @@
 var NeuralNetworkJS = require("../index.js");
-var NeuralNowUtils = require('neural-now-utils');
 var NeuralNetwork = NeuralNetworkJS.NeuralNetwork;
 var Trainer = NeuralNetworkJS.Trainer;
 
-NeuralNowUtils.Text.toBigramVector("This is a test.", function (vector) {
-  // setup trainer and gather data
-  var trainer = new Trainer();
-  var trainX = [vector];
-  var trainY = [[1]];
+// setup trainer and gather data
+var trainer = new Trainer();
+var trainX = trainer.data.getTrainingX();
+var trainY = trainer.data.getTrainingY();
+var testX = trainX;
+var testY = trainY;
 
-  // architect neural network
-  var neuralNetwork = new NeuralNetwork();
-  neuralNetwork.input.size = trainX[0].length;
-  neuralNetwork.input.setActivation("linear");
-  neuralNetwork.addHiddenLayer(3, "sigmoid");
-  neuralNetwork.addHiddenLayer(3, "sigmoid");
-  neuralNetwork.output.size = trainY[0].length;
-  neuralNetwork.output.setActivation("sigmoid");
-  neuralNetwork.generate();
+// architect neural network
+var neuralNetwork = new NeuralNetwork();
+neuralNetwork.input.size = trainX[0].length;
+neuralNetwork.input.setActivation("linear");
+neuralNetwork.addHiddenLayer(3, "hyperbolic-tangent");
+neuralNetwork.addHiddenLayer(3, "hyperbolic-tangent");
+neuralNetwork.output.size = trainY[0].length;
+neuralNetwork.output.setActivation("hyperbolic-tangent");
+neuralNetwork.generate();
 
-  // train
-  trainer.x = trainX;
-  trainer.y = trainY;
-  trainer.neuralNetwork = neuralNetwork;
-  trainer.scalar = 0.000001;
-  trainer.lambda = 0.001;
-  trainer.goal = 0.001;
-  trainer.train(10000);
-  trainer.writeToFile();
+// train
+trainer.neuralNetwork = neuralNetwork;
+trainer.generateFromFile("cosine.json");
+console.log(trainer.neuralNetwork.forward([[Math.PI], [Math.PI / 2]]));
+trainer.scalar = 0.000001;
+trainer.lambda = 0.00001;
+trainer.goal = 0.0001;
+trainer.train(100000);
+trainer.writeToFile();
 
-  // print results
-  console.log("\nTraining data set results");
-  trainer.printResults(trainX.splice(0,10), trainY.splice(0,10));
-});
+// print results
+console.log("\nTraining data set results");
+trainer.printResults(trainX.splice(0,10), trainY.splice(0,10));
+
+//console.log("\nTesting data set results");
+//trainer.printResults(testX, testY);
