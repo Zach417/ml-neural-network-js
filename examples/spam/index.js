fd6426659a1e094ee532716947e71d2096abfea0
Zach Allen <zallen@pension-consultants.com>
2017-01-22 15:33:26 -0600

    fixed train and test data split

--- examples/spam/index.js
+++ examples/spam/index.js
@@ -8,43 +8,34 @@ var Trainer = NeuralNetworkJS.Trainer;
 
 function train (trainX, trainY, testX, testY) {
   var trainer = new Trainer();
-  //trainer.generateFromFile("spam.json");
 
-  NeuralNowUtils.Text.toBigramVector("Check out the amazing offer!!", function (vector) {
-    // // architect neural network
-    // var neuralNetwork = new NeuralNetwork();
-    // neuralNetwork.input.size = trainX[0].length;
-    // neuralNetwork.input.setActivation("linear");
-    // neuralNetwork.addHiddenLayer(10, "sigmoid");
-    // neuralNetwork.addHiddenLayer(10, "sigmoid");
-    // neuralNetwork.addHiddenLayer(10, "sigmoid");
-    // neuralNetwork.output.size = trainY[0].length;
-    // neuralNetwork.output.setActivation("sigmoid");
-    // neuralNetwork.generate();
-    // trainer.neuralNetwork = neuralNetwork;
+  // // architect neural network
+  // var neuralNetwork = new NeuralNetwork();
+  // neuralNetwork.input.size = trainX[0].length;
+  // neuralNetwork.input.setActivation("linear");
+  // neuralNetwork.addHiddenLayer(3, "sigmoid");
+  // neuralNetwork.addHiddenLayer(3, "sigmoid");
+  // neuralNetwork.output.size = trainY[0].length;
+  // neuralNetwork.output.setActivation("sigmoid");
+  // neuralNetwork.generate();
+  // trainer.neuralNetwork = neuralNetwork;
 
-    trainer.generateFromFile("new-neural-network.json");
+  trainer.generateFromFile("spam-classification.json");
 
-    var x = [vector];
-    var y = [[0,1]];
-    console.log(trainer.neuralNetwork.forward(x));
+  trainer.x = trainX;
+  trainer.y = trainY;
+  trainer.scalar = 0.000001;
+  trainer.lambda = 0.0001;
+  trainer.goal = 0.0;
+  trainer.train(1000);
 
-    trainer.x = trainX;
-    trainer.y = trainY;
-    trainer.scalar = 0.8;
-    trainer.lambda = 0.01;
-    trainer.goal = 0.0;
-    trainer.train(100);
+  // print results
+  console.log("\nTraining data set results");
+  trainer.printResults(trainX, trainY);
 
-    // print results
-    console.log("\nTraining data set results");
-    trainer.printResults(trainX, trainY);
-
-    console.log("\nTesting data set results");
-    trainer.printResults(testX, testY);
-  });
+  console.log("\nTesting data set results");
+  trainer.printResults(testX, testY);
 }
-
 
 var trainX = JSON.parse(fs.readFileSync(__dirname + "/trainX.json"));
 var trainY = JSON.parse(fs.readFileSync(__dirname + "/trainY.json"));
@@ -52,31 +43,51 @@ var testX = JSON.parse(fs.readFileSync(__dirname + "/testX.json"));
 var testY = JSON.parse(fs.readFileSync(__dirname + "/testY.json"));
 train(trainX, trainY, testX, testY);
 
-// var idx = 0;
 // getData(function (data) {
-//   var x = [];
-//   var y = [];
+//   var testX = [];
+//   var testY = [];
+//   var trainX = [];
+//   var trainY = [];
 //
-//   var epochs = data.x.length;
-//   for (var i = 0; i < epochs; i++) {
-//     NeuralNowUtils.Text.toBigramVector(data.x[i], function (vector) {
-//       x.push(vector);
-//       y.push(data.y[idx]);
-//       idx++;
-//       if (idx == epochs - 1) {
-//         var length = Math.ceil(x.length * .75);
-//         var trainX = x.splice(0,length);
-//         var trainY = y.splice(0,length);
-//         var testX = x;
-//         var testY = y;
-//
-//         fs.writeFileSync(__dirname + "/trainX.json", JSON.stringify(trainX), "utf-8");
-//         fs.writeFileSync(__dirname + "/trainY.json", JSON.stringify(trainY), "utf-8");
-//         fs.writeFileSync(__dirname + "/testX.json", JSON.stringify(testX), "utf-8");
-//         fs.writeFileSync(__dirname + "/testY.json", JSON.stringify(testY), "utf-8");
-//
-//         train(trainX, trainY, testX, testY);
-//       }
-//     });
+//   for (var i = 0; i < data.x.length; i++) {
+//     var vector = NeuralNowUtils.Text.toBigramVector(data.x[i]);
+//     if (Math.random() < .25) {
+//       testX.push(vector);
+//       testY.push(data.y[i]);
+//     } else {
+//       trainX.push(vector);
+//       trainY.push(data.y[i]);
+//     }
 //   }
+//
+//   var hamCount = 0;
+//   var spamCount = 0;
+//   for (var i = 0; i < testY.length; i++) {
+//     if (testY[i][1] === 1) {
+//       spamCount++;
+//     } else {
+//       hamCount++;
+//     }
+//   }
+//   console.log(testY[0]);
+//   console.log(hamCount, spamCount);
+//
+//   var hamCount = 0;
+//   var spamCount = 0;
+//   for (var i = 0; i < trainY.length; i++) {
+//     if (trainY[i][1] === 1) {
+//       spamCount++;
+//     } else {
+//       hamCount++;
+//     }
+//   }
+//   console.log(trainY[0]);
+//   console.log(hamCount, spamCount);
+//
+//   fs.writeFileSync(__dirname + "/trainX.json", JSON.stringify(trainX), "utf-8");
+//   fs.writeFileSync(__dirname + "/trainY.json", JSON.stringify(trainY), "utf-8");
+//   fs.writeFileSync(__dirname + "/testX.json", JSON.stringify(testX), "utf-8");
+//   fs.writeFileSync(__dirname + "/testY.json", JSON.stringify(testY), "utf-8");
+//
+//   train(trainX, trainY, testX, testY);
 // });
