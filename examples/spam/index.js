28314b1f7ea384a21de2cd1e92c8f0a25b615287
Zach Allen <zallen@pension-consultants.com>
2017-01-22 18:06:48 -0600

    changed data y output

--- examples/spam/index.js
+++ examples/spam/index.js
@@ -9,18 +9,18 @@ var Trainer = NeuralNetworkJS.Trainer;
 function train (trainX, trainY, testX, testY) {
   var trainer = new Trainer();
 
-  // // architect neural network
-  // var neuralNetwork = new NeuralNetwork();
-  // neuralNetwork.input.size = trainX[0].length;
-  // neuralNetwork.input.setActivation("linear");
-  // neuralNetwork.addHiddenLayer(3, "sigmoid");
-  // neuralNetwork.addHiddenLayer(3, "sigmoid");
-  // neuralNetwork.output.size = trainY[0].length;
-  // neuralNetwork.output.setActivation("sigmoid");
-  // neuralNetwork.generate();
-  // trainer.neuralNetwork = neuralNetwork;
+  // architect neural network
+  var neuralNetwork = new NeuralNetwork();
+  neuralNetwork.name = "spam-classifier";
+  neuralNetwork.input.size = trainX[0].length;
+  neuralNetwork.input.setActivation("linear");
+  neuralNetwork.addHiddenLayer(10, "sigmoid");
+  neuralNetwork.output.size = trainY[0].length;
+  neuralNetwork.output.setActivation("sigmoid");
+  neuralNetwork.generate();
+  trainer.neuralNetwork = neuralNetwork;
 
-  trainer.generateFromFile("spam-classification.json");
+  // trainer.generateFromFile("spam-classification.json");
 
   trainer.x = trainX;
   trainer.y = trainY;
@@ -59,30 +59,6 @@ train(trainX, trainY, testX, testY);
 //       trainY.push(data.y[i]);
 //     }
 //   }
-//
-//   var hamCount = 0;
-//   var spamCount = 0;
-//   for (var i = 0; i < testY.length; i++) {
-//     if (testY[i][1] === 1) {
-//       spamCount++;
-//     } else {
-//       hamCount++;
-//     }
-//   }
-//   console.log(testY[0]);
-//   console.log(hamCount, spamCount);
-//
-//   var hamCount = 0;
-//   var spamCount = 0;
-//   for (var i = 0; i < trainY.length; i++) {
-//     if (trainY[i][1] === 1) {
-//       spamCount++;
-//     } else {
-//       hamCount++;
-//     }
-//   }
-//   console.log(trainY[0]);
-//   console.log(hamCount, spamCount);
 //
 //   fs.writeFileSync(__dirname + "/trainX.json", JSON.stringify(trainX), "utf-8");
 //   fs.writeFileSync(__dirname + "/trainY.json", JSON.stringify(trainY), "utf-8");
